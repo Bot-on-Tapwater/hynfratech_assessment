73574756d8b695bdf4d610039d70d03e03632c75
Brandon Munda <mundabrandon@outlook.com>
2024-09-07 22:40:41 +0300

    chore: Add api routes for the new views

--- vm_management/urls.py
+++ vm_management/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('register/', views.register, name='register'),
-    # path('login/', views.login, name='login'),
-    # path('', views.home, name='home'),
+    path('', views.vm_list, name='vm_list'),
+    path('create/', views.create_vm, name='create_vm'),
+    path('delete/<int:vm_id>/', views.delete_vm, name='delete_vm'),
+    path('backup/<int:vm_id>/', views.backup_vm, name='backup_vm'),
 ]
