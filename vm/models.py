bb5439f13a5c67fcf1fd79917949f0524c9f48d4
Brandon Munda <mundabrandon@outlook.com>
2024-09-07 14:46:16 +0300

    Initial commit

---
+++ vm/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserRole(models.TextChoices):
+    ADMIN = 'Admin', 'Admin'
+    STANDARD_USER = 'Standard User', 'Standard User'
+    GUEST = 'Guest', 'Guest'
+
+class CustomUser(AbstractUser):
+    role = models.CharField(max_length=50, choices=UserRole.choices, default=UserRole.GUEST)
+
+# Add any other fields you need for the user model.
