c6e48342bd34ca60c26baf57ac53afc3313c2d80
Brandon Munda <mundabrandon@outlook.com>
2024-09-12 01:47:07 +0300

    feat: Include paths for authentication (JWTs)

--- hynfratech_assessment/urls.py
+++ hynfratech_assessment/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("vm_management/", include("vm_management.urls")),
     path("accounts/", include("accounts.urls")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
