7b27d9d93e7065b88a8e98b8e48f9cf5b77afc55
Brandon Munda <mundabrandon@outlook.com>
2024-09-12 21:17:54 +0300

    chore: Add url for logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('', views.home, name='home'),
     path('google-complete/', views.google_complete, name='google-complete'),
 ]
