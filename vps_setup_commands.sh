2a5a70eb6cbf56719c294700ee402fe487d3ebb9
Brandon Munda <mundabrandon@outlook.com>
2024-09-21 17:39:26 +0300

    chore: Update commands list

--- vps_setup_commands.sh
+++ vps_setup_commands.sh
@@ -57,3 +57,6 @@ GRANT ALL PRIVILEGES ON DATABASE hynfratech TO botontapwater;
 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+
+scp -P 2112 <local_file_path> lab@102.209.68.78:/home/lab/hynfratech_assessment/
