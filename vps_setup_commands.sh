50070e85e3202e422cacee77a6f21824f43438f5
Brandon Munda <mundabrandon@outlook.com>
2024-09-22 08:08:51 +0300

    chore: Remove unused commands

--- vps_setup_commands.sh
+++ vps_setup_commands.sh
@@ -1,62 +1,55 @@
+# Update package list
+sudo apt update
+
+
+# Docker installation
 sudo apt-get install docker
-sudo apt-get install docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
+
+# Docker-compose installation (Option 1)
+sudo apt-get install docker-compose
+
+
+# Docker-compose installation (Option 1)
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Kubernetes installation | Kubectl
 snap install kubectl --classic
 
+# Minikube installation (Required for creating kubernetes clusters locally)
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-# minikube start
+minikube start
 
-# sudo apt install virtualbox
-
-# wget -4 -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
-# echo "deb [signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-# sudo apt-get update
-# sudo apt-get install virtualbox-6.1
-
+# Virtualbox installation (Gives access to vboxmanage command line tool)
 sudo apt-get install virtualbox-7.1
-
-
-
 sudo service virtualbox start
-sudo usermod -aG docker 
+
+# Postgres installation (Not necessary since the db is run as a docker container)
 sudo apt-get install -y postgresql postgresql-contrib
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 
-sudo apt-get install certbot
-sudo apt install certbot python3-certbot-nginx
-sudo certbot --nginx
-sudo certbot renew --dry-run
-sudo certbot --nginx -d hynfrapersonal.botontapwater.com --non-interactive --agree-tos --email mundabrandon@outlook.com --redirect
+    # Run these in psql (sudo su postgres; psql;) - Not necessary DB is setup in container
+    CREATE DATABASE hynfratech;
+    CREATE USER botontapwater WITH PASSWORD 'TwoGreen1.';
+    GRANT ALL PRIVILEGES ON DATABASE hynfratech TO botontapwater;
 
+# Nginx reverse proxy installation (Necessary for deployment)
+sudo systemctl start nginx
+sudo systemctl enable nginx
 sudo systemctl restart nginx
 
-# sudo apt-get install -y conntrack
+# Certbot installation and setup (For installing SSL certificates)
+sudo certbot --nginx
+sudo certbot renew --dry-run
+sudo certbot --nginx -d hynfratech.botontapwater.com --non-interactive --agree-tos --email mundabrandon@outlook.com --redirect
 
-# VERSION="v1.27.0" # Update to the latest version if needed
-# wget https://github.com/kubernetes-sigs/cri-tools/releases/download/${VERSION}/crictl-${VERSION}-linux-amd64.tar.gz
-# tar -zxvf crictl-${VERSION}-linux-amd64.tar.gz
-# sudo mv crictl /usr/local/bin/
-
-sudo apt update
-gunicorn --workers 3 hynfratech_assessment.wsgi:application
-# sudo apt install postgresql postgresql-contrib
-
+# Allows nginx to access and serve static files in our repo
 sudo chmod +x /home/lab
 sudo chmod +x /home/lab/hynfratech_assessment
 
-
-# sudo systemctl start postgresql
-# sudo systemctl enable postgresql
-
-CREATE DATABASE hynfratech;
-CREATE USER botontapwater WITH PASSWORD 'TwoGreen1.';
-GRANT ALL PRIVILEGES ON DATABASE hynfratech TO botontapwater;
-
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-
+# Command to let you send any missing files to the repo
 scp -P 2112 <local_file_path> lab@102.209.68.78:/home/lab/hynfratech_assessment/
